698748fff64f6c7e9ada82a04eb4ffb581eeec09
rahulcode60 <116915274+rahulcode60@users.noreply.github.com>
2023-07-18 17:07:00 +0530

    Add files via upload

---
+++ src/utils/HandleApi.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+
+
+const baseUrl ="http://localhost:5000";
+
+const getAllToDo = (setToDo)=>{
+    axios.get(baseUrl).then(({data})=>{
+        console.log("data --->",data);
+        setToDo(data);
+    })
+}
+
+const addToDo = (text,setText,setToDo)=>{
+    axios
+    .post(`${baseUrl}/save`,{text})
+    .then((data)=>{
+        console.log(data)
+        setText("");
+        getAllToDo(setToDo);
+    }).catch((err) =>{
+        console.log(err);
+    }) 
+}
+
+const updateToDo = (toDoId,text,setToDo,setText,setIsUpdating)=>{
+    axios
+    .put(`${baseUrl}/update`,{_id:toDoId,text:text})
+    .then((data)=>{
+        setText("");
+        setIsUpdating(false);
+        getAllToDo(setToDo);
+    }).catch((err) =>{
+       
+        console.log(err);
+    }) 
+}
+
+const deleteToDo = (_id,setToDo)=>{
+    console.log(_id);
+    axios
+    .delete(`${baseUrl}/delete`,{ data: { _id } })
+    .then((data)=>{
+        console.log(data)
+        getAllToDo(setToDo);
+    }).catch((err) =>{
+       
+        console.log(err);
+    }) 
+}
+
+
+export {getAllToDo,addToDo,updateToDo,deleteToDo}
